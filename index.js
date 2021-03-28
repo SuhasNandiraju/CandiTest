eee1e3deaed4f07e74e378bfc6814cea03b54941
nkanchinadam <53796958+nkanchinadam@users.noreply.github.com>
2021-03-27 20:18:05 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,71 @@
+#!/usr/bin/nodejs
+
+// -------------- load packages -------------- //
+// INITIALIZATION STUFF
+
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json())
+
+var https = require('http');
+var hbs = require('hbs');
+const { read } = require('fs');
+const { REFUSED } = require('dns');
+
+app.set('view engine', 'hbs');
+app.use(express.static('static'));
+
+// -------------- express 'get' handlers -------------- //
+// These 'getters' are what fetch your pages
+
+app.get('/', function(req, res) {
+    res.render('input');
+    console.log('input page loaded');
+})
+
+app.post('/generate', make_api_request, function(req, res) {
+    var obj = res.locals.obj;
+    var data = [];
+    for(i = 0; i < obj.length; i++) {
+        data[i] = {'number': i + 1, 'questions': obj[i]};
+    }
+    res.render('output', {'tests': data});
+    console.log('This is the main page.');
+});
+
+function make_api_request(req, res, next) {
+    var url = 'http://127.0.0.1:5000/get_test';
+    console.log('url: ' + url);
+    var options =  { headers : {
+            'User-Agent': 'request'
+        },
+        'tests': req.body.numTests,
+        'questions': req.body.questionsText
+    }
+
+    https.get(url, options, function(response) {
+        var rawData = '';
+        response.on('data', function(chunk) {
+            rawData += chunk;
+        });
+
+        response.on('end', function() {
+            obj = JSON.parse(rawData);
+            res.locals.obj = obj;
+            console.log(obj);
+            next();
+        });
+
+    }).on('error', function(e) {
+        console.error(e);
+    });
+}
+
+// -------------- listener -------------- //
+// // The listener is what keeps node 'alive.' 
+
+var listener = app.listen(process.env.PORT || 80, process.env.HOST || "0.0.0.0", function() {
+    console.log("Express server started");
+});
